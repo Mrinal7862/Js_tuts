cc1dd4acd9760cd65658e5db608fe7cd0b054dda
Mrinal7862 <codemaxter473@gmail.com>
2024-06-30 07:16:29 +0000

    data_type

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,25 @@
-console.log("Anju loves Minu")
+const accountId = 14423;
+
+let accoutEmailId = "Mrinal@devnath12312";
+
+var accountSmarter = "12345"
+
+jammer = "Varanasi";
+
+
+// accountId = 339912; 
+
+accountSmarter = 998800
+// console.log(accountSmarter);
+
+accoutEmailId = "Anju@gmail.com";
+
+accountSmarter = "889900";
+
+let accountState;
+
+// console.log(accountId)
+
+// console.log(accoutEmailId);
+
+console.table([accountId, accoutEmailId, accountSmarter, jammer, accountState])
