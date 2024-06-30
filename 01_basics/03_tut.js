7dc33921a5bb774eea583e481f6c79985b0c3815
Mrinal7862 <codemaxter473@gmail.com>
2024-06-30 10:31:01 +0000

    3_tutorial

---
+++ 01_basics/03_tut.js
@@ -0,0 +1,26 @@
+// *********************************Operations****************************************
+
+let value = 3;
+
+let negvalue = -value
+
+console.log(value + negvalue)
+
+let str1 =  "hello"; 
+let str2 = " Ramesh";
+
+console.log(str1 + str2)
+
+let letter = "1" + 2;
+
+console.log(letter);
+console.log(typeof letter);
+
+console.log(0 <= 3);
+
+console.log(0 == '0');
+
+console.log(0 === '0')
+
+console.log(21 >= 33);
+
