738d43da83337b54741ed956226588f7ee129afc
Mrinal7862 <codemaxter473@gmail.com>
2024-06-30 10:30:24 +0000

    data_typesum

---
+++ 01_basics/data_typesum_4.js
@@ -0,0 +1,18 @@
+//Primitive data types
+
+//There are 7 primitive data typer = Number, String, Boolean, Undefined, Null, bigInt, symbol
+
+const isLoggedIn = false; 
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+
+const anotherId = Symbol('123')
+
+const compare = id === anotherId;
+
+const bigNumber = BigInt(3141541414741471141441698412698526852n);
+
+//Refrence Data types 62
+ 
