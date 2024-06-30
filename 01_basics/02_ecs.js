50fa613d9fdc7493ba42593a3ecb5c7765368bd0
Mrinal7862 <codemaxter473@gmail.com>
2024-06-30 07:16:39 +0000

    dtype_conversion

---
+++ 01_basics/02_ecs.js
@@ -0,0 +1,15 @@
+// let score = false;
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isloggedIn = 1; 
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+
+console.log(booleanIsLoggedIn)
