36e2da86a139130dad284089affbf895ca8ed2c1
Mrinal7862 <codemaxter473@gmail.com>
2024-07-07 12:34:10 +0530

    Update objects_2.js

--- 02_basics/objects_2.js
+++ 02_basics/objects_2.js
@@ -1,37 +1,64 @@
-// const tinderUser = new Object() //It is the single ton object
-const tinderUser = {}
+// // const tinderUser = new Object() //It is the single ton object
+// const tinderUser = {}
 
 
-tinderUser.id = "123abcc";
-tinderUser.name = "Sammy";
-tinderUser.isLoggedIn = false;
+// tinderUser.id = "123abcc";
+// tinderUser.name = "Sammy";
+// tinderUser.isLoggedIn = false;
 
-// console.log(tinderUser);
+// // console.log(tinderUser);
 
-const regularUser = {
-    email: "some@gmail.com",
-    fullname: {
-        username: {
-            firstName: "Mrinal",
-            lastName: "Devnath"
-        }
-    }
+// const regularUser = {
+//     email: "some@gmail.com",
+//     fullname: {
+//         username: {
+//             firstName: "Mrinal",
+//             lastName: "Devnath"
+//         }
+//     }
+// }
+
+// // console.log(regularUser.fullname.username.firstName)
+
+// const obj1 = {1:"a", 2: "b"}
+// const obj2 = {3:"c", 4: "d"}
+// const obj4 = {5:"e", 6: "f"}
+
+// // const obj3 = {obj1, obj2}
+
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4) //{} Creates an empty object which is target and the rest of the params are source it is created to store all the objects into a new object
+
+// console.log(obj3)
+
+// const objSpread = {...obj1, ...obj2,...obj3}
+
+// console.log(objSpread)
+
+// Destructuring 
+const course ={
+    coursename : "js hindi",
+    courseIstructor : "Mrinal Devanth",
+    coursePrice : '12k'
 }
 
-// console.log(regularUser.fullname.username.firstName)
+const {courseIstructor: instructor} = course
 
-const obj1 = {1:"a", 2: "b"}
-const obj2 = {3:"c", 4: "d"}
-const obj4 = {5:"e", 6: "f"}
+console.log(instructor);
 
-// const obj3 = {obj1, obj2}
+const navbar = () =>{
+    
+}
+
+// navbar(company: Mrinal)
 
 
-const obj3 = Object.assign({}, obj1, obj2, obj4) //{} Creates an empty object which is target and the rest of the params are source it is created to store all the objects into a new object
 
-console.log(obj3)
+//A.P.I.s 
 
-const objSpread = {...obj1, ...obj2,...obj3}
+{
 
-console.log(objSpread)
-
+    {},
+    {},
+    {}
+}
